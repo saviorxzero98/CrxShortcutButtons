aef953cdc7d98b8c7fd461178b0e1a2729e1ae27
Marty Chen <marty_chen@mail.gss.com.tw>
2022-06-28 10:44:14 +0800

    加入 開啟視窗、分頁、無痕視窗

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,10 +1,51 @@
 $(function()
 {
-	$('li').click(function()
-	{
-		if ($(this).data('href'))
-		{
-			chrome.tabs.create({ url: $(this).data('href') });
-		}
-	});
+    let openUrlInNewTab = (url) => {
+        chrome.tabs.create({ 
+            url: url ?? 'chrome://newtab/'
+        });
+    }
+
+    let openUrlInNewWindow = (url) => {
+        chrome.windows.create({
+            url: url ?? 'chrome://newtab/'
+        })
+    }
+
+    let openIncognitoWindow = (url) => {
+        chrome.windows.create({
+            url: url ?? 'chrome://newtab/',
+            incognito: true
+        })
+    }
+
+    $('.tab-item').click(function()
+    {
+        if ($(this).data('href'))
+        {
+            openUrlInNewTab($(this).data('href'));
+
+            window.close();
+        }
+    });
+
+    $('.window-item').click(function()
+    {
+        if ($(this).data('href'))
+        {
+            openUrlInNewWindow($(this).data('href'));
+
+            window.close();
+        }
+    });
+
+    $('.incognito-item').click(function()
+    {
+        if ($(this).data('href'))
+        {
+            openIncognitoWindow($(this).data('href'));
+
+            window.close();
+        }
+    });
 });
